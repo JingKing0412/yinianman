dbe9c9721783f15349409223daad71adfe456f57
JingKing0412 <zjx13027661518@126.com>
2025-04-14 23:06:56 +0800

    Update yinianxuewangye.py

--- yinianxuewangye.py
+++ yinianxuewangye.py
@@ -24,7 +24,7 @@ feature_names = [
 ]
 
 # Streamlit 名字
-st.title("Heart Disease Predictor")
+st.title("Chronic coronary syndrome 1-year mortality predictor")
 model_path1 = 'min_max_scaleryinianxe12345678.pkl'
 min_max_scaler = joblib.load(model_path1 )
 # User inputs
@@ -63,7 +63,7 @@ if st.button("Predict"):
 
     # SHAP Explanation for class 0
     st.subheader(
-        f"Based on feature values, predicted probability of one-year mortality is: **{probability_class_0:.1f}%**")
+        f"Based on feature values, the predicted 1-year mortality rate of chronic coronary syndrome is: **{probability_class_0:.1f}%**")
     explainer_shap = shap.KernelExplainer(model.predict_proba, background_data_scaled)
     shap_values = explainer_shap(pd.DataFrame(features, columns=feature_names))
 
